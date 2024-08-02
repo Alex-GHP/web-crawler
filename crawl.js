850c26ca9768a1d655f6286d37098e1bad0c13b5
Alex-GHP <alex.morariu.dev@gmail.com>
2024-08-02 08:38:17 +0300

    getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,4 +1,28 @@
-const { hostname } = require("os")
+const { JSDOM } = require('jsdom')
+
+const getURLsFromHTML = (htmlBody, baseURL) => {
+    const URLs = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === "/") {
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                URLs.push(urlObj.href)
+            } catch (err) {
+                console.log(`Error with relative URL: ${err.message}`)
+            }
+        } else {
+            try {
+                const urlObj = new URL(linkElement.href)
+                URLs.push(urlObj.href)
+            } catch (err) {
+                console.log(`Error with absolute URL: ${err.message}`)
+            }
+        }
+    } 
+    return URLs
+}
 
 const normalizeURL = (urlString) => {
     const urlObj = new URL(urlString)
@@ -10,5 +34,6 @@ const normalizeURL = (urlString) => {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
