00059dbbb33a0ba8325df1267d9a42d1b7d29028
Alex-GHP <alex.morariu.dev@gmail.com>
2024-08-02 14:40:12 +0300

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,64 +1,97 @@
-const { resolvePlugin } = require('@babel/core')
 const { JSDOM } = require('jsdom')
 
-const crawlPage = async (currentURL) => {
-    console.log(`Actively crawling: ${currentURL}`)
+function normalizeURL(url) {
+  const urlObj = new URL(url)
+  let fullPath = `${urlObj.host}${urlObj.pathname}`
+  if (fullPath.slice(-1) === '/') {
+    fullPath = fullPath.slice(0, -1)
+  }
+  return fullPath
+}
 
-    try {
-        const resp = await fetch(currentURL)
+function getURLsFromHTML(html, baseURL) {
+  const urls = []
+  const dom = new JSDOM(html)
+  const anchors = dom.window.document.querySelectorAll('a')
 
-        if (resp.status > 399) {
-            console.log(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-            return
-        }
+  for (const anchor of anchors) {
+    if (anchor.hasAttribute('href')) {
+      let href = anchor.getAttribute('href')
 
-        const contentType = resp.headers.get("content-type")
-        if (!contentType.includes("text/html")) {
-            console.log(`Non html response, content type: ${contentType}, on page: ${currentURL}`)
-            return
-        }
-
-        console.log(await resp.text())
-    } catch (err) {
-        console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`)
+      try {
+        // convert any relative URLs to absolute URLs
+        href = new URL(href, baseURL).href
+        urls.push(href)
+      } catch(err) {
+        console.log(`${err.message}: ${href}`)
+      }
     }
+  }
+
+  return urls
 }
 
-const getURLsFromHTML = (htmlBody, baseURL) => {
-    const URLs = []
-    const dom = new JSDOM(htmlBody)
-    const linkElements = dom.window.document.querySelectorAll('a')
-    for (const linkElement of linkElements) {
-        if (linkElement.href.slice(0, 1) === "/") {
-            try {
-                const urlObj = new URL(`${baseURL}${linkElement.href}`)
-                URLs.push(urlObj.href)
-            } catch (err) {
-                console.log(`Error with relative URL: ${err.message}`)
-            }
-        } else {
-            try {
-                const urlObj = new URL(linkElement.href)
-                URLs.push(urlObj.href)
-            } catch (err) {
-                console.log(`Error with absolute URL: ${err.message}`)
-            }
-        }
-    } 
-    return URLs
+async function fetchHTML(url) {
+  let res
+  try {
+    res = await fetch(url)
+  } catch (err) {
+    throw new Error(`Got Network error: ${err.message}`)
+  }
+
+  if (res.status > 399) {
+    throw new Error(`Got HTTP error: ${res.status} ${res.statusText}`)
+  }
+
+  const contentType = res.headers.get('content-type')
+  if (!contentType || !contentType.includes('text/html')) {
+    throw new Error(`Got non-HTML response: ${contentType}`)
+  }
+
+  return res.text()
 }
 
-const normalizeURL = (urlString) => {
-    const urlObj = new URL(urlString)
-    hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
-        return hostPath.slice(0, -1)
-    }
-    return hostPath
+// use default args to prime the first call
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+  // if this is an offsite URL, bail immediately
+  const currentURLObj = new URL(currentURL)
+  const baseURLObj = new URL(baseURL)
+  if (currentURLObj.hostname !== baseURLObj.hostname) {
+    return pages
+  }
+
+  // use a consistent URL format
+  const normalizedURL = normalizeURL(currentURL)
+
+  // if we've already visited this page
+  // just increase the count and don't repeat
+  // the http request
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++
+    return pages
+  }
+
+  // initialize this page in the map
+  // since it doesn't exist yet
+  pages[normalizedURL] = 1
+
+  // fetch and parse the html of the currentURL
+  console.log(`crawling ${currentURL}`)
+  let html = ''
+  try {
+    html = await fetchHTML(currentURL)
+  } catch (err) {
+    console.log(`${err.message}`)
+    return pages
+  }
+
+  // recur through the page's links
+  const nextURLs = getURLsFromHTML(html, baseURL)
+  for (const nextURL of nextURLs) {
+    pages = await crawlPage(baseURL, nextURL, pages)
+  }
+
+  return pages
 }
 
-module.exports = {
-    normalizeURL,
-    getURLsFromHTML,
-    crawlPage
-}
+module.exports = { normalizeURL, getURLsFromHTML, crawlPage }
