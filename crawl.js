e632d0fe58b834a853d02386716097dc4d5c1568
Alex-GHP <alex.morariu.dev@gmail.com>
2024-08-02 09:03:52 +0300

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,4 +1,28 @@
+const { resolvePlugin } = require('@babel/core')
 const { JSDOM } = require('jsdom')
+
+const crawlPage = async (currentURL) => {
+    console.log(`Actively crawling: ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399) {
+            console.log(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`Non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+}
 
 const getURLsFromHTML = (htmlBody, baseURL) => {
     const URLs = []
@@ -35,5 +59,6 @@ const normalizeURL = (urlString) => {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
